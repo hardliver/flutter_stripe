66d9e618c01613c4f195d1a99f9e3f9d3d6a6d5f
Neil Ye <neil.ye@cloud-interactive.com>
2021-02-17 11:49:28 +0800

    Part 1 / 3 - Home & Existing Cards Pages UI

---
+++ lib/pages/existing-cards.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
+import 'package:flutter_stripe/services/payment-service.dart';
+
+class ExistingCardsPage extends StatefulWidget {
+  @override
+  _ExistingCardsPageState createState() => _ExistingCardsPageState();
+}
+
+class _ExistingCardsPageState extends State<ExistingCardsPage> {
+  List cards = [
+    {
+      'cardNumber': '4242424242424242',
+      'expiryDate': '04/24',
+      'cardHolderName': 'Muhammad Ahsan Ayaz',
+      'cvvCode': '424',
+      'showBackView': false,
+    },
+    {
+      'cardNumber': '3566002020360505',
+      'expiryDate': '04/23',
+      'cardHolderName': 'Tracer',
+      'cvvCode': '123',
+      'showBackView': false,
+    }
+  ];
+
+  payViaExistingCard(BuildContext context, card) {
+    var response = StripeService.payWithExistingCard(
+      amount: '150',
+      currency: 'USD',
+      card: card,
+    );
+    if (response.success == true) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(
+            SnackBar(
+              content: Text(response.message),
+              duration: new Duration(milliseconds: 1200),
+            ),
+          )
+          .closed
+          .then((_) => Navigator.pop(context));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Choose existing card'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: cards.length,
+          itemBuilder: (BuildContext context, int index) {
+            var card = cards[index];
+            return InkWell(
+              onTap: () {
+                payViaExistingCard(context, card);
+              },
+              child: CreditCardWidget(
+                cardNumber: card['cardNumber'],
+                expiryDate: card['expiryDate'],
+                cardHolderName: card['cardHolderName'],
+                cvvCode: card['cvvCode'],
+                showBackView: false, //true when you want to show cvv(back) view
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/payment-service.dart
@@ -0,0 +1,28 @@
+class StripeTransactionResponse {
+  String message;
+  bool success;
+  StripeTransactionResponse({this.message, this.success});
+}
+
+class StripeService {
+  static String apiBase = 'https://api.stripe.com//v1';
+  static String secret = '';
+
+  static init() {}
+
+  static StripeTransactionResponse payWithExistingCard(
+      {String amount, String currency, card}) {
+    return new StripeTransactionResponse(
+      message: 'Transaction successful',
+      success: true,
+    );
+  }
+
+  static StripeTransactionResponse payWithNewCard(
+      {String amount, String currency}) {
+    return new StripeTransactionResponse(
+      message: 'Transaction successful',
+      success: true,
+    );
+  }
+}
